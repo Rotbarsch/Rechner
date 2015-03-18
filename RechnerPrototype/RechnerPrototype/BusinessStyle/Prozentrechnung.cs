c29a7e8de8b2d951ab84c8a2378f0e72bdb88935
Rotbarsch <rhamke@googlemail.com>
2015-03-18 10:37:02 +0100

    Create Prozentrechnung.cs

---
+++ RechnerPrototype/RechnerPrototype/BusinessStyle/Prozentrechnung.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RechnerPrototype.BusinessStyle
+{
+    public partial class Prozentrechnung : Form
+    {
+        public Prozentrechnung()
+        {
+            InitializeComponent();
+        }
+    }
+}
